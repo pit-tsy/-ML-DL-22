5dbcd80cd4c0dbeac5e997ae91584f9a64f01765
pit-tsy <113280889+pit-tsy@users.noreply.github.com>
2022-09-11 10:59:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import train
 import generate
 
 train.fit()
-with open('generated_text', 'w', encoding='windows-1251') as file:
+with open('generated_text.txt', 'w', encoding='windows-1251') as file:
     file.write(generate.gen())
